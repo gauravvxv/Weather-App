830b811ac9fef6597447680f5cb7a9c22e3b4459
Gaurav Sharma <gauraxvx0410@gmail.com>
2024-07-15 15:50:40 +0530

    starting

---
+++ weather-forecast/src/components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Weather = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">Hello</h1>
+      <input type="text" name="search" id="search" placeholder='search ' />
+    </div>
+  )
+}
+
+export default Weather
